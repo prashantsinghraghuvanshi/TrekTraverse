252d31d1379e889d334ecc4a0d8e8ce979b16502
Prashant Singh <singhprashant0314@gmail.com>
2024-03-30 19:09:26 +0530

    implemented router

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,10 @@
+import PageNav from "../components/PageNav";
+
+export default function PageNotFound() {
+  return (
+    <div>
+      <PageNav />
+      <h1>Page not Found :(</h1>
+    </div>
+  );
+}
